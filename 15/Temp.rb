0d34e77ed2a726bc47bc754ceb5f4eac54646fe2
jqhou2 <jqhou2@iflytek.com>
2024-08-21 16:59:23 +0800

    修改算法题

--- 15/Temp.rb
+++ 15/Temp.rb
@@ -1,47 +1,53 @@
-# frozen_string_literal: true
+# 注意left <= right  是小于等于
+# 因为去重的时候
+#  if sum > 0
+#         right -= 1
+#       elsif sum < 0
+#         left += 1
+#       else
+# 导致了 [-5, -5, -4, -4, -4, -2, -2, -2, 0, 0, 0, 1, 1, 3, 4, 4]
+# 0,0,0 处在边界判断上  导致left == right
 
-# 三数之和
+
+
 # @param {Integer[]} nums
 # @return {Integer[][]}
 def three_sum(nums)
-  nums.sort!
   result = []
+  nums = nums.sort
+
   (0...nums.size).each do |i|
-    right = nums.size - 1
-    left = i + 1
-
-    if nums[0] > 0
-      []
-    end
-
+    left, right = i + 1, nums.size - 1
     if i > 0 && nums[i] == nums[i - 1]
       next
     end
-    while right > left
-    sum = nums[i] + nums[left] + nums[right]
+
+    while left <= right
+      sum = nums[i] + nums[left] + nums[right]
+
+      while nums[right] == nums[right -1]
+        right -= 1
+      end
+      while nums[left] == nums[left + 1]
+        left += 1
+      end
+
       if sum > 0
         right -= 1
       elsif sum < 0
         left += 1
       else
         result << [nums[i], nums[left], nums[right]]
-
-        while right > left && nums[right] == nums[right - 1]
-          right -= 1
-        end
-        while right > left && nums[left] == nums[left + 1]
-          left += 1
-        end
-        right -= 1
         left += 1
-
+        right -= 1
       end
+
     end
 
   end
   result
-
 end
 
-nums = [-1, 0, 1, 2, -1, -4]
+nums = [-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0]
+
 p three_sum(nums)
